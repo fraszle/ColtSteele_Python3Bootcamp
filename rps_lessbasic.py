226fea385df56bcd7433b8c263284245b0b6759d
Victoria Dmyterko <44418940+fraszle@users.noreply.github.com>
2020-07-30 16:49:42 -0700

    Slightly more advanced Rock, Paper, Scissors program
    This "more advanced" program uses randint to assign the computer a choice. It will take user input, then compare the input to the computer's choice using conditional logic to determine a winner. The user input is made case-insensitive by using the lower() function to make the input all lowercase letters.

---
+++ rps_lessbasic.py
@@ -0,0 +1,39 @@
+import random
+
+print("...rock...")
+print("...paper...")
+print("...scissors...")
+
+player = input("Enter your choice: ")
+player = player.lower()
+
+comp = random.randint(1, 3)
+if comp == 1:
+	comp = "rock"
+	print("The computer plays: rock")
+elif comp == 2:
+	comp = "paper"
+	print("The computer plays: paper")
+elif comp == 3:
+	comp = "scissors"
+	print("The computer plays: scissors")
+
+if player == comp:
+	print("It's a tie!")
+elif player == "rock":
+	if comp == "scissors":
+		print("Player wins!")
+	elif comp == "paper":
+		print("Computer wins!")
+elif player == "paper":
+	if comp == "rock":
+		print("Player wins!")
+	elif comp == "scissors":
+		print("Computer wins!")
+elif player == "scissors":
+	if comp == "paper":
+		print("Player wins!")
+	elif comp == "rock":
+		print("Computer wins!")
+else:
+	print("Something went wrong! :(")
